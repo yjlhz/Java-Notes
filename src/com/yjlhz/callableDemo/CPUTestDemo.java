5144db8de08130f73127aa30f73bc5325cdad5ee
yjlhz <2386987985@qq.com>
2021-03-02 00:19:07 +0800

    获取计算机CPU几线程

---
+++ src/com/yjlhz/callableDemo/CPUTestDemo.java
@@ -0,0 +1,7 @@
+package com.yjlhz.callableDemo;
+
+public class CPUTestDemo {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
